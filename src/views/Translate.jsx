d0fb494bd99c3e600ecf0941b31742ff4a1219c6
soniamartinezz <sonia93mp@gmail.com>
2024-04-23 21:49:36 +0200

    update function toggle and text placeholder + css

--- src/views/Translate.jsx
+++ src/views/Translate.jsx
@@ -5,8 +5,19 @@ import Footer from "../components/Footer";
 
 function Translate() {
     const [textAreaValue, setTextAreaValue] = useState('');
-    const textAreaRef = useRef(null);
     const [language, setLanguage] = useState('Español ➔ Euskera');
+    const [placeholderText, setPlaceholderText] = useState('Escribe los textos de tu Web');
+    const textAreaRef = useRef(null);
+
+    const handleToggle = (newLanguage, textButtonValue) => {
+        setLanguage(newLanguage);
+        setPlaceholderText(textButtonValue === 'Español ➔ Euskera' ? 'Idatzi zure Webgunearen testuak' : 'Escribe los textos de tu Web');
+        handleTranslate(textButtonValue);
+    };
+
+    const handleTextAreaChange = (event) => {
+        setTextAreaValue(event.target.value);
+    };
 
     const handleTranslate = async () => {
         const sourceLanguage = language === 'Español ➔ Euskera' ? 'es' : 'eu';
@@ -15,20 +26,9 @@ function Translate() {
         try {
             const response = await axios.get(`http://localhost:3000/?word=${textAreaRef.current.value}&source=${sourceLanguage}&target=${targetLanguage}`);
             setTextAreaValue(response.data.translated_text);
-            console.log(response.data.translated_text);
         } catch (error) {
             console.error(error);
         }
-    };
-
-
-    const handleToggle = (newLanguage) => {
-        setLanguage(newLanguage);
-        handleTranslate();
-    };
-
-    const handleTextAreaChange = (event) => {
-        setTextAreaValue(event.target.value);
     };
 
     return(
@@ -39,7 +39,7 @@ function Translate() {
                     <p>Demostración visual de como gracias a Neskatila puedes transformar instantáneamente los textos de tu sitio Web o aplicación al euskera, o al español, con solo un clic en un botón de alternancia (toggle). Solo necesitas especificar el idioma de origen de tu Web o app y Neskatila se encargará del resto. Esta demostración te mostrará cómo Neskatila evitará la necesidad de introducir los textos en un segundo idioma en tu programación”</p>
                     <div className="translate">
                         <Toggle onToggle={handleToggle} />
-                        <textarea ref={textAreaRef} value={textAreaValue} onChange={handleTextAreaChange} placeholder="Escribe los textos de tu Web"></textarea>
+                        <textarea ref={textAreaRef} value={textAreaValue} onChange={handleTextAreaChange} placeholder={placeholderText}></textarea>
                     </div>
                 </section>
             </main>
