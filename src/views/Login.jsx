4d4101b6bf67daaf7875bd72b9d00ef867e6211f
soniamartinezz <sonia93mp@gmail.com>
2024-04-26 23:37:14 +0200

    login + register done

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,68 +2,48 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate(); // Mover useNavigate aquí
 
-  const validateEmail = (value) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(value)) {
-      setEmailError('Ingrese un correo electrónico válido');
-    } else {
-      setEmailError('');
-    }
-  };
-
-  const handleEmailChange = (event) => {
-    const { value } = event.target;
-    setEmail(value);
-    validateEmail(value);
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-    // Validación de la contraseña
-    if (event.target.value.trim() === '') {
-      setPasswordError('Ingrese su contraseña');
-    } else {
-      setPasswordError('');
-    }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Validación de la contraseña
-    if (password.trim() === '') {
-      setPasswordError('Ingrese su contraseña');
-      return;
-    } else {
-      setPasswordError('');
+    const data = { username, password };
+
+    try {
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        navigate('/normas-uso'); // Utilizar navigate aquí
+      } else {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      console.log('Mensaje de error recibido:', error.message); // Agregar esta línea para imprimir el mensaje de error recibido
+      if (error.message === 'El nombre de usuario no existe') {
+        setError('El usuario ingresado no existe. Por favor, regístrese primero.');
+      } else {
+        setError('Error al iniciar sesión. Por favor, inténtalo de nuevo.');
+      }
     }
-
-    // Aquí podrías agregar la lógica para iniciar sesión, como enviar los datos al servidor
-    console.log('Email:', email);
-    console.log('Password:', password);
-
-    // Limpia los campos después del envío
-    setEmail('');
-    setPassword('');
-
-    const response = await fetch('http://localhost:3000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    const navigate = useNavigate();
-    if (response.ok) {
-      navigate('/normas-uso');
-    }
-
   };
 
   return (
@@ -71,20 +51,19 @@ function LoginForm() {
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={handleEmailChange} placeholder="Ingrese su correo electrónico" required />
-          {emailError && <p style={{ color: 'red' }}>{emailError}</p>}
+          <label>Username:</label>
+          <input type="text" value={username} onChange={handleUsernameChange} placeholder="Ingrese su nombre de usuario" required />
         </div>
         <div>
           <label>Contraseña:</label>
           <input type="password" value={password} onChange={handlePasswordChange} placeholder="Ingrese su contraseña" required />
-          {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
         </div>
         <button type="submit">Iniciar sesión</button>
       </form>
       <Link to="/registro">
         <button type="button">Registrarse</button>
       </Link>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
 }
